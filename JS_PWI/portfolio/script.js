cc2e54d8533f86cef23c3bc1492b9d02bb546745
Andrzej Jurga <andrzej.jurga@uczen.zsk.poznan.pl>
2020-11-09 23:55:51 +0100

    k

--- JS_PWI/portfolio/script.js
+++
@@ -1,9 +0,0 @@
-function play(vid){
-    var video = $(vid);
-    video.get(0).play();
-}
-
-function pause(vid){
-    var video = $(vid);
-    video.get(0).pause();
-}
--- README.md
+++
@@ -1,3 +0,0 @@
-# zsk
-Repozytorium zawierające materiały z pracowni w
-Zespole Szkół Komunikacji im. Hipolita Cegielskiego
